8c997026ded7379f4985fe11df0b3c37d889e909
robakerson <robakerson@gmail.com>
2021-07-14 15:29:48 -0500

    add animation to drum presses

--- Drum Kit/index.js
+++ Drum Kit/index.js
@@ -11,11 +11,13 @@ function handleClick(){
 
 // Play sound based on which button is pressed
 makeSound(this.innerHTML);
+buttonAnimation(this.innerHTML);
 }
 
 // make sound for button press
 document.addEventListener("keydown", function(event){
 makeSound(event.key);
+buttonAnimation(event.key);
 })
 
 function makeSound(key){
@@ -36,6 +38,14 @@ function makeSound(key){
       break;
     default:console.log(key);
 }
+}
+
+function buttonAnimation(key){
+    var activeButton= document.querySelector("." + key);
+    activeButton.classList.add("pressed");
+    setTimeout(function(){
+      activeButton.classList.remove("pressed");
+    }, 50)
 }
 
 // Constructor function
