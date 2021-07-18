02b44637cf9ad2595bace81a648a60dd9c7c20ca
robakerson <robakerson@gmail.com>
2021-07-17 21:02:23 -0500

    many jquery methods added then commented out

--- jquery/index.js
+++ jquery/index.js
@@ -1,2 +1,63 @@
+// Add class to element with jquery
+// $("h1").addClass("big-money small-money");
 
-$("h1").addClass("big-money small-money");
+//Set attribute of href for all anchor tags
+// $("a").attr("href", "https://www.yahoo.com");
+
+//change color of h1 when clicked, event listener
+// $("h1").click(function(){
+//   $("h1").css("color","red");
+// })
+
+//change color of h1 when button clicked
+// $("button").click(function(){
+//   $("h1").css("color","purple");
+// })
+
+// literal keylogger!
+// $(document).keydown(function(event){
+//   console.log(event.key);
+// })
+
+// Change H1 to = key pressed. live!
+// $(document).keydown(function(event){
+//   $("h1").text(event.key);
+// })
+
+// change color of h1 to purple when moused over
+// $("h1").on("mouseover", function(){
+//   $("h1").css("color","purple");
+// })
+
+//add button before/after h1 element
+// $("h1").before("<button>new</button>");
+// $("h1").after("<button>new</button>");
+
+//add button inside beginning/end h1 tag
+// $("h1").prepend("<button>new</button>");
+// $("h1").append("<button>new</button>");
+
+// hide/show h1 when button pressed
+// $("button").on("click", function(){
+//   $("h1").toggle();
+// })
+
+// hide/show h1 when button pressed but it fades in/out
+// $("button").on("click", function(){
+//   $("h1").fadeToggle();
+// })
+
+// hide/show h1 when button pressed but slide up and down
+// $("button").on("click", function(){
+//   $("h1").slideToggle();
+// })
+
+// animate things to happen
+// $("button").on("click", function(){
+//   $("h1").animate({opacity: 0.5});
+// })
+
+//chain animations!
+$("button").on("click", function(){
+  $("h1").slideUp().slideDown().animate({opacity:0.5})
+})
