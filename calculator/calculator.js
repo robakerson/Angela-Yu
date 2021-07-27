6308799fb5e7d6c12d4b63050a6012525ef1d3e5
robakerson <robakerson@gmail.com>
2021-07-26 23:29:02 -0500

    Done?

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,11 +1,27 @@
 
 const express = require('express');
-const app = express();
+const bodyParser = require('body-parser');
 
+//require express.js
+const app = express();
+//Expresss use bodyparser so we can use values POSTED from user in our script
+app.use(bodyParser.urlencoded({extended: true}));
+
+//start server on port 3000
 app.listen(3000, function(){
   console.log("Server started on port 3000");
 })
 
+// when user goes to root of server, serve them index.html
 app.get("/", function(req, res){
-  res.send("Hello World!");
+  res.sendFile(__dirname + "/index.html");
+})
+
+app.post("/", function(req,res){
+// Calculate the sum
+var num1 = Number(req.body.num1);
+var num2 = Number(req.body.num2);
+var result = num1 + num2;
+// Send result to user
+  res.send("Your result is: " + result);
 })
