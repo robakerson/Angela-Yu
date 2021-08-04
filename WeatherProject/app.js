394793e1db22d3c928c2f6bc7197f4998e9be9f1
robakerson <robakerson@gmail.com>
2021-08-03 22:12:48 -0500

    Weather App

--- WeatherProject/app.js
+++ WeatherProject/app.js
@@ -1,24 +1,39 @@
 const express = require('express');
 const https = require('https');
+const bodyParser= require('body-parser');
 
 const app = express();
 
-app.get('/', function(req,res){
 
+app.use(bodyParser.urlencoded({extended:true}));
 
-  const url = "https://api.openweathermap.org/data/2.5/weather?q=Cedar Rapids&appid=[key]"
+app.get('/', function(req, res){
+  res.sendFile(__dirname + "/index.html");
+})
 
-  https.get(url, function(response){
-    console.log(response);
-    response.on('data', function(data){
-      const weatherData= JSON.parse(data);
-      const temp = weatherData.main.temp;
+app.post('/', function(req, res){
+    const query = req.body.cityName;
+    const apiKey = [key];
+    const units = "imperial";
+    const url = "https://api.openweathermap.org/data/2.5/weather?q=" + query + "&units=" + units + "&appid=" + apiKey;
 
-      console.log(temp);
+    https.get(url, function(response){
+      response.on('data', function(data){
+        const weatherData= JSON.parse(data);
+        const temp = weatherData.main.temp;
+        const weatherDescription = weatherData.weather[0].description;
+        const icon = weatherData.weather[0].icon;
+        const imageURL = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
+
+        console.log(temp);
+        console.log(weatherDescription);
+        res.write('<p> The weather is currently ' + weatherDescription + '</p>')
+        res.write('<h1> The temperature in ' + query + ' is '+ temp +' degrees</h1>');
+        res.write('<img src=' +imageURL+ '>');
+        res.send()
+      });
     });
-  });
-
-  res.send('server is up and running');
+    // res.send('server is up and running');
 })
 
 app.listen(3000, function(){
