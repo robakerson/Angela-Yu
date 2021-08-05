e063b3f10d7c2beb8073adb4c08bdbcc2319c0cc
robakerson <robakerson@gmail.com>
2021-08-04 22:30:49 -0500

    public css to get via server

--- Newsletter-Signup/app.js
+++ Newsletter-Signup/app.js
@@ -5,10 +5,17 @@ const request = require('request');
 
 const app = express();
 
+// provide path for static files so HTML can use CSS with relative URL
+app.use(express.static("public"));
+
 app.listen(3000, function(){
   console.log("Server UP BOI");
 });
 
 app.get("/", function(req,res){
-  res.send("HEY");
+  res.sendFile(__dirname + "/signup.html");
 });
+
+app.post("/", function(req, res){
+
+})
